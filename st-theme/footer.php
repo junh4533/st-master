8776edf7f436f2cc03a0555e8d1a8e11d5c551d9
Jun <junh4533@gmail.com>
2020-07-02 20:30:11 -0400

    add contact page

--- st-theme/footer.php
+++ st-theme/footer.php
@@ -13,7 +13,7 @@
         <p class="nav-link">
             <a href="mailto:info.savingtakeouts@gmail.com">info.savingtakeouts@gmail.com</a>
             &nbsp;|&nbsp; <a href="<?php echo get_site_url(); ?>/contact-us">Support</a> &nbsp;|
-            &nbsp; <a href="<?php echo get_site_url(); ?>/privacy-policy">Privacy Policy</a>
+            &nbsp; <a href="<?php echo get_site_url(); ?>/privacy-policy">Privacy Policy</a> |
             &nbsp; <a href="<?php echo get_site_url(); ?>/terms-and-conditions">Terms</a>
         </p>
         <!-- copy right -->
---
+++ st-theme/page-contact-us.php
@@ -0,0 +1,19 @@
+<!-- ENSURE THAT THE TEMPLATE NAME IS CORRECT  -->
+<?php /* Template Name: Contact Us */ ?>
+<?php get_header();?>
+
+<section id="contact">
+    <h2 class="d-block text-center"> Contact Us </h2>
+    <br>
+    <div class="container jumbotron">
+        <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2.js"></script>
+        <script>
+            hbspt.forms.create({
+                portalId: '7988040',
+                formId: '8e65c71f-4fb8-4eeb-85d6-9a66044d3539',
+            })
+        </script>
+    </div>
+</section>
+
+<?php get_footer();?>
