c9245d8c414fe636b41e4de2b3d76e4cb68bb730
Jun <junh4533@gmail.com>
2020-07-22 14:01:11 -0400

    test localwp repo

---
+++ st-theme/privacy-policy.php
@@ -0,0 +1,168 @@
+<!-- ENSURE THAT THE TEMPLATE NAME IS CORRECT  -->
+<?php /* Template Name: Privacy Policy */ ?> 
+<?php get_header(); ?>
+
+ <!-- PRIVACY POLICY SECTION -->
+ <section id="privacy-policy">
+        <div class="container">
+            <h2>Privacy Policy</h2>
+            <p class="mt-4">
+                At SavingTakeouts, we are dedicated to safeguarding and preserving your privacy when
+                visiting our site or communicating electronically with us. This Privacy Policy, together with
+                our terms of use, explains what happens to any personal data that you provide to us, or that
+                we collect from you when you are on this site. We do update this Policy from time to time so
+                please do return and review this Policy regularly.
+            </p>
+            <p class="my-3">
+                <b>Definitions </b> <br><br>
+                SavingTakeouts values your privacy. In the below policies, whenever we use the term “user”
+                or “you” it refers to the person using the website. Whenever we use the term
+                “SavingTakeouts”, “we”, “this website”, it refers to the creators of the website.
+                The term “Personal Information” refers to information that identifies you personally, alone or
+                in combination with other information available to us. Examples of Personal Information
+                include your name, address, and email address.
+            </p>
+            <p class="my-3">
+                <b>Information We Collect </b> <br><br>
+                We collect all information you provide us with such as when you register for an account
+                including but not limited to name, address, IP address, email address, physical address,
+                contact number, and how you use our services.
+                We also record and collect information when: <br> <br>
+                ● Any survey or form you complete or any customer service you receive. <br>
+                ● Information you provide on any one of our communication channels or third party
+                platforms. <br><br>
+                We use the data we collect from you for lawful purposes such as fulfilling our obligations to
+                you under a contract and to keep our records updated and other purposes such as
+                marketing. We will only ever use your data for the purposes which we collect.
+                SavingTakeouts will share your personal information with third parties when applicable such
+                as assessment purposes.
+                We also use Google Analytics on our website. Google Analytics is an analytics software and
+                helps us to analyze, monitor, and improve our website based on the user’s actions. We do
+                this to improve the end-user experience.
+                Through the use of Google Analytics, we may collect data such as what page you are
+                visiting on our site, how long you spend, and the actions you take on our site.
+                You have the right to withdraw from any marketing correspondence from us, to do so contact
+                us.
+                You may be able to choose to register, log-on, or enhance your profile on this Site using
+                functionalities provided by a third-party social media platform, such as Facebook. By doing
+                this, you are asking the third-party social media platform to send us information from your
+                profile on that platform. We treat that information as we do any other information you give to
+                us when you register, log-on, or enhance your profile. After you have connected, the third
+
+                party may also send us additional information from your profile that allows us to offer certain
+                functionalities on the pages you view.
+            </p>
+            <p class="my-3">
+                <b>Cookies </b> <br><br>
+                SavingTakeouts uses cookies to enhance user experience and for analytics purposes.
+                Cookies are text files that are stored in your browser. This is so we can personalize our
+                website to your needs and remember your preferences.
+                We also use cookies for analytics purposes, mainly Google Analytics. We use this so we can
+                better understand our users and see how we can improve on our website and provide a
+                better user experience.
+                Cookies are only stored on browsers for 12 months. Generally, if you do not wish to receive
+                cookies, you may set your browser to reject cookies or to alert you when a cookie is placed
+                on your computer. Although you are not required to accept cookies when you visit this Site,
+                you may be unable to use all of the functionality of this Site if your browser rejects our
+                cookies.
+            </p>
+            <p class="my-3">
+                <b>Third-Party Links</b> <br><br>
+                You may find links to third party websites on our website. These websites should have their
+                own privacy policies which you should check. We do not accept any responsibility or liability
+                for their policies whatsoever as we have no control over them.
+            </p>
+            <p class="my-3">
+                <b>Email Opt-In</b> <br><br>
+
+                We use an email newsletter mailing list to inform our customers and subscribers about
+                products or services supplied by this website. You can subscribe by opting in and signing up
+                using your email address. Some subscriptions may be manually processed through a prior
+                written agreement with the user.
+                Our email marketing campaigns may contain tracking facilities within the actual email. We
+                may track and monitor activities such as the opening of emails, the clicking of links within an
+                email, and the sharing of emails. This information is used to refine future email campaigns
+                and supply the user with more relevant content based on their activity.
+                If you would like to opt out of our mailing list, then please contact our support team.
+            </p>
+            <p class="my-3">
+                <b> Retargeting</b> <br><br>
+
+                We may use remarketing services to advertise on third party websites (including Google and
+                Facebook) to previous visitors to our site. Remarketing utilizes cookies on the user’s
+                computer to serve ads from us.
+                This could mean if you visited our site to see our services, you may see an ad from us on
+                Facebook. Any data collected are in accordance with our privacy policy and
+                Google/Facebook privacy policy.
+            </p>
+            <p class="my-3">
+                <b>Social Media</b> <br><br>
+
+                Communication, engagement, and actions taken through external social media platforms
+                that this website and it&#39;s owners participate in are custom to the terms and conditions as well
+                as the privacy policies held with each social media platform respectively.
+                Users are advised to use social media platforms wisely and communicate/engage upon
+                them with due care and caution in regard to their own privacy and personal details. This
+                website nor it&#39;s owners will ever ask for personal or sensitive information through social
+                media platforms and encourage users wishing to discuss sensitive details to contact them
+                through primary communication channels such as by telephone or email.
+                This website may use social sharing buttons that help share web content directly from web
+                pages to the social media platform in question. Users are advised before using such social
+                sharing buttons that they do so at their own discretion and note that the social media
+                platform may track and save your request to share a web page respectively through your
+                social media platform account.
+            </p>
+            <p class="my-3">
+                <b>Personal Information</b> <br><br>
+                When you use our website you agree to use using and storing your personal information. We
+                may share your personal information with other third-party companies or law enforcement to
+                further check whether your data is correct.
+                Your personal data is very important to us, and we store your data responsibly. We will
+                never rent, sell, or give your personal data to any third party without your consent.
+                We also may track the actions you take on our website. We do this so we can better
+                optimize our website.
+                SavingTakeouts may share your personal information with external third parties such as law
+                enforcement, service providers, affiliates or to comply with any legal requests.
+                We may store financial details for faster checkout however these are encrypted and secure.
+                Under no circumstances should information be submitted or uploaded by or about children
+                under the age of 13 without the consent of their parent or guardian.
+            </p>
+            <p class="my-3">
+                <b>Keeping your Data Secure</b> <br><br>
+
+                We maintain reasonable administrative, physical, and technological measures to protect the
+                confidentiality and security of Personal Information you submit on or through this Site. For
+                example, we use SSL encryption to protect your credit card information as it travels over the
+                Internet. Unfortunately, no website, server, or database is completely secure. We, therefore,
+                cannot guarantee that your Personal Information will not be disclosed, misused, or lost by
+                accident or by the unauthorized acts of others.
+
+            </p>
+            <p class="my-3">
+                <b>Rights</b> <br><br>
+
+                You have the right to ask us not to store or process your personal data for any purposes
+                including marketing purposes. We will inform you if we intend to use your data for such
+                purposes or if we intend to disclose your personal information to any third party for any
+                purpose.
+
+                Your account details are kept secure and are never in any circumstances shared with any
+                third party. Once services are complete your details are deleted.
+                Under the GDPR you have the right to be informed, the right to access your personal data,
+                and receive a copy of your personal information. You also have the right to rectify any of
+                your personal data that you may find to be incorrect and the right to remove, delete, and
+                object.
+            </p>
+            <p class="my-3">
+                <b>Effective Date of Policy</b> <br><br>
+
+                This policy is effective and current as of June 2020. We may make changes to this policy
+                and process and systems without further warning. We may also update this privacy policy to
+                reflect updates.
+                Maintaining this privacy policy of customer information is of utmost importance to us and it
+                helps build stronger relationships, goodwill and increase confidence.
+            </p>
+        </div>
+    </section>
+
+  <?php get_footer() ?>
