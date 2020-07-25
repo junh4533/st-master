295f911f1bda390fcf17cbc16deb8dd3edb40014
Ivy Tong <ivytong@Ivys-MacBook-Air.local>
2020-07-24 23:23:37 -0400

    Test promo page

---
+++ st-theme/page-promo-page.php
@@ -0,0 +1,13 @@
+<?php /* Template Name: Promo Page */ ?>
+<?php get_header();?>
+<section class="text-center" id="promo-page">
+    <h3>Five FREE Websites for a Limited Time Only!!</h3>
+    <p class="mt-4">This promotion is offered on a first come first serve basis. We will only provide free hosting for the first FIVE clients. Web design and development still remains free for all small businesses.</p>
+    <div class="container mt-5">
+
+        
+            
+
+    </div>
+</section>
+<?php get_footer();?>
