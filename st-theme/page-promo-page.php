b6078df992543abb59dfa1d3b468e90bd5158bb6
Jun <junh4533@gmail.com>
2020-07-26 00:19:05 -0400

    add circles for promo page

--- st-theme/page-promo-page.php
+++ st-theme/page-promo-page.php
@@ -2,13 +2,46 @@
 <?php get_header();?>
 <section class="text-center" id="promo-page">
     <h3>Five FREE Websites for a Limited Time Only!!</h3>
-    <p class="mt-4">This promotion is offered on a first come first serve basis. We will only provide free hosting for the first FIVE clients. Web design and development still remains free for all small businesses.</p>
-    <div class="container mt-5">
+    <p class="mt-4">This promotion is offered on a first come first serve basis. We will only provide free hosting for
+        the first FIVE clients. Web design and development still remains free for all small businesses.</p>
+    <div class="container-fluid mt-5">
         <div class="row justify-content-center">
-            <div class="col-12 col-md-6 col-lg-4 mb-5">
-                <div class="features-item">
-                Free hosting (typically costs $100+)
+            <div class="col-12 col-lg-8">
+                <div class="row justify-content-center">
+                    <div class="col-6 col-lg-4 mb-5">
+                        <div class="features-item">
+                            Free hosting (typically costs $100+)
+                        </div>
+                    </div>
+                    <div class="col-6 col-lg-4 mb-5">
+                        <div class="features-item">
+                            Web Content
+                        </div>
+                    </div>
+                    <div class="col-6 col-lg-4 mb-5">
+                        <div class="features-item">
+                            Maintenance
+                        </div>
+                    </div>
+                    <div class="col-6 col-lg-4 mb-5">
+                        <div class="features-item">
+                            Marketing
+                        </div>
+                    </div>
+                    <div class="col-6 col-lg-4 mb-5">
+                        <div class="features-item">
+                            Subject to domain cost (~$10 per year)
+                        </div>
+                    </div>
+                    <div class="col-6 col-lg-4 mb-5">
+                        <div class="features-item">
+                            Click here to APPLY NOW!
+                        </div>
+                    </div>
                 </div>
+            </div>
+            <div class="col-12 col-lg-4">
+                INSERT GRAPHIC HERE
             </div>
         </div>
     </div>
