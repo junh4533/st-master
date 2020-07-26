4c0783862142f8dce297a9feb270f67a344e442c
Ivy Tong <ivytong@ivys-air.myfiosgateway.com>
2020-07-26 00:22:33 -0400

    Add Ivy's promo page changes

--- st-theme/page-promo-page.php
+++ st-theme/page-promo-page.php
@@ -1,14 +1,19 @@
+
 <?php /* Template Name: Promo Page */ ?>
 <?php get_header();?>
 <section class="text-center" id="promo-page">
-    <h3>Five FREE Websites for a Limited Time Only!!</h3>
+    <h3>Five <span id="blueText">FREE </span>Websites for a Limited Time Only!!</h3>
     <p class="mt-4">This promotion is offered on a first come first serve basis. We will only provide free hosting for the first FIVE clients. Web design and development still remains free for all small businesses.</p>
     <div class="container mt-5">
         <div class="row justify-content-center">
             <div class="col-12 col-md-6 col-lg-4 mb-5">
-                <div class="features-item">
+                <div class="col 6">
                 Free hosting (typically costs $100+)
                 </div>
+
+                <div class="col 6">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/idea.png">
+               </div>
             </div>
         </div>
     </div>
