c54b9dd2a3d8d360a95e29a23d82afca423286d2
Jun <junh4533@gmail.com>
2020-07-26 01:08:11 -0400

    add programming png

--- st-theme/page-promo-page.php
+++ st-theme/page-promo-page.php
@@ -1,13 +1,15 @@
-
 <?php /* Template Name: Promo Page */ ?>
 <?php get_header();?>
 <section class="text-center" id="promo-page">
-    <h3>Five <span id="blueText">FREE </span>Websites for a Limited Time Only!!</h3>
+    <h3>Five <span class="blueText">FREE </span>Websites for a Limited Time Only!!</h3>
     <p class="mt-4">This promotion is offered on a first come first serve basis. We will only provide free hosting for
         the first FIVE clients. Web design and development still remains free for all small businesses.</p>
     <div class="container-fluid mt-5">
         <div class="row justify-content-center">
-            <div class="col-12 col-lg-8">
+            <div class="col-12 col-lg-6 mb-5">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/programming.png">
+            </div>
+            <div class="col-12 col-lg-6 mb-5 d-flex align-items-center">
                 <div class="row justify-content-center">
                     <div class="col-6 col-lg-4 mb-5">
                         <div class="features-item">
@@ -36,13 +38,10 @@
                     </div>
                     <div class="col-6 col-lg-4 mb-5">
                         <div class="features-item">
-                            Click here to APPLY NOW!
+                            <a class="blueText" href="https://forms.gle/9AR2Q6d3EMm1Lnqt7" target="_blank">Click here to APPLY NOW!</a>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-12 col-lg-4">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/programming.png">
             </div>
         </div>
     </div>
