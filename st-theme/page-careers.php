ce71ff51f7144b77eaf6f199ae632c45a4e74edd
Jun <junh4533@gmail.com>
2020-09-15 10:36:27 -0400

    finish first iteration

---
+++ st-theme/page-careers.php
@@ -0,0 +1,9 @@
+<!-- ENSURE THAT THE TEMPLATE NAME IS CORRECT  -->
+<?php /* Template Name: Careers */ ?>
+<?php get_header();?>
+
+<section class="text-center" id="careers">
+    <h1 class="large-heading">We're recruiting!</h1>
+</section>
+
+<?php get_footer();?>
